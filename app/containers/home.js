bc7be92e6a0cd7daa87b3ca9a1b1f47c85a87810
forrest23 <forrest23@126.com>
2016-08-03 13:40:15 +0800

    主页动态设置

--- app/containers/home.js
+++ app/containers/home.js
@@ -9,114 +9,84 @@ export default class Home extends Component {
     super(props);
 
     this.state = {
-      rows: [
+      firstRows: [
         { image: require('../assets/yntz.png'), text: '院内通知' },
         { image: require('../assets/ygztc.png'), text: '员工直通车' }
       ],
 
-      rows2: [
+      secondRows: [
         { image: require('../assets/hzgl.png'), text: '患者管理' },
         { image: require('../assets/mzpb.png'), text: '门诊排班' },
         { image: require('../assets/ydjc.png'), text: '移动决策' },
         { image: require('../assets/sspb.png'), text: '手术排班' },
-        { image: require('../assets/huizgl.png'), text: '会诊管理' }
+        { image: require('../assets/huizgl.png'), text: '会诊管理' },
+        { image: require('../assets/yxgs.png'), text: '医学公式' },
+        { image: require('../assets/wjzgl.png'), text: '危急值' },
+        { image: require('../assets/yxzx.png'), text: '医学资讯' },
+        { image: require('../assets/xjpx.png'), text: '先进评选' },
+        { image: require('../assets/more.png'), text: '更多...' }
       ],
     }
   }
 
+
   render() {
     var halfSize = Dimensions.get('window').width / 2;
-    var rowViews = this.state.rows.map(function (r, j) {
+    var thirdSize = Dimensions.get('window').width / 3;
+    var firstRowViews = this.state.firstRows.map(function (r, j) {
       return (
         <TouchableOpacity style={[styles.row, { width: halfSize }]} key={j}>
-          <View style={styles.part_left}><Image style={styles.image} source={r.image}></Image></View>
-          <View style={styles.part_right}><Text style={styles.text}>{r.text}</Text></View>
+          <View style={styles.flex_1}><Image style={styles.image} source={r.image}></Image></View>
+          <View style={styles.flex_1}><Text style={styles.text}>{r.text}</Text></View>
         </TouchableOpacity>
       );
     });
 
-    var rowViews2 = this.state.rows2.map(function (r, j) {
-      return (
-        <TouchableOpacity style={[styles.row, { width: halfSize }]} key={j}>
-          <View style={styles.part_left}><Image style={styles.image} source={r.image}></Image></View>
-          <View style={styles.part_right}><Text style={styles.text}>{r.text}</Text></View>
-        </TouchableOpacity>
-      );
+    var secondRowViews = this.state.secondRows.map(function (r, j, arry) {
+      if (j == 0 || j == 5) {
+        return (
+          <View style={{ flex: 1 }} key={j}>
+            <TouchableOpacity style={[styles.row2]}>
+              <View ><Image style={styles.image3} source={r.image}></Image></View>
+              <View ><Text style={styles.text3}>{r.text}</Text></View>
+            </TouchableOpacity>
+          </View>
+        );
+      }
+      else {
+        return (
+          <TouchableOpacity style={[styles.row, { width: thirdSize }]} key={j}>
+            <View style={styles.flex_1}><Image style={styles.image2} source={r.image}></Image></View>
+            <View style={styles.flex_1}><Text style={styles.text2}>{r.text}</Text></View>
+          </TouchableOpacity>
+        );
+      }
     });
 
     return (
       <ScrollView style={styles.container}>
         <Swiper></Swiper>
-        <View style={styles.list}>
-          {rowViews}
+        <View style={styles.block1}>
+          {firstRowViews}
         </View>
 
-        <View style={styles.list2}>
-          <View style={{ flex: 1 }}>
-            <TouchableOpacity style={[styles.row2]}>
-              <View ><Image style={styles.image3} source={require('../assets/hzgl.png') }></Image></View>
-              <View ><Text style={styles.text3}>患者管理</Text></View>
-            </TouchableOpacity>
-          </View>
-          <View style={{ flex: 2 }}>
-            <View style={{ flexDirection: 'row', height: 63, borderBottomWidth: 1, borderColor: '#d0d0d0' }}>
-              <TouchableOpacity style={[styles.row, styles.part_left]}>
-                <View style={styles.part_left}><Image style={styles.image2} source={require('../assets/mzpb.png') }></Image></View>
-                <View style={styles.part_right}><Text style={styles.text2}>门诊排班</Text></View>
-              </TouchableOpacity>
-
-              <TouchableOpacity style={[styles.row, styles.part_left]}>
-                <View style={styles.part_left}><Image style={styles.image2} source={require('../assets/ydjc.png') }></Image></View>
-                <View style={styles.part_right}><Text style={styles.text2}>移动决策</Text></View>
-              </TouchableOpacity>
-            </View>
-
-            <View style={{ flexDirection: 'row', height: 63 }}>
-              <TouchableOpacity style={[styles.row, styles.part_left]}>
-                <View style={styles.part_left}><Image style={styles.image2} source={require('../assets/sspb.png') }></Image></View>
-                <View style={styles.part_right}><Text style={styles.text2}>手术排班</Text></View>
-              </TouchableOpacity>
-
-              <TouchableOpacity style={[styles.row, styles.part_left]}>
-                <View style={styles.part_left}><Image style={styles.image2} source={require('../assets/huizgl.png') }></Image></View>
-                <View style={styles.part_right}><Text style={styles.text2}>会诊管理</Text></View>
-              </TouchableOpacity>
-            </View>
+        <View style={styles.block2}>
+          {secondRowViews[0]}
+          <View style={styles.row3}>
+            {secondRowViews[1]}
+            {secondRowViews[2]}
+            {secondRowViews[3]}
+            {secondRowViews[4]}
           </View>
         </View>
 
-
-        <View style={styles.list2}>
-          <View style={{ flex: 1 }}>
-            <TouchableOpacity style={[styles.row2]}>
-              <View ><Image style={styles.image3} source={require('../assets/yxgs.png') }></Image></View>
-              <View ><Text style={styles.text3}>医学公式</Text></View>
-            </TouchableOpacity>
-          </View>
-          <View style={{ flex: 2 }}>
-            <View style={{ flexDirection: 'row', height: 63, borderBottomWidth: 1, borderColor: '#d0d0d0' }}>
-              <TouchableOpacity style={[styles.row, styles.part_left]}>
-                <View style={styles.part_left}><Image style={styles.image2} source={require('../assets/wjzgl.png') }></Image></View>
-                <View style={styles.part_right}><Text style={styles.text2}>危急值</Text></View>
-              </TouchableOpacity>
-
-              <TouchableOpacity style={[styles.row, styles.part_left]}>
-                <View style={styles.part_left}><Image style={styles.image2} source={require('../assets/yxzx.png') }></Image></View>
-                <View style={styles.part_right}><Text style={styles.text2}>医学资讯</Text></View>
-              </TouchableOpacity>
-            </View>
-
-            <View style={{ flexDirection: 'row', height: 63 }}>
-              <TouchableOpacity style={[styles.row, styles.part_left]}>
-                <View style={styles.part_left}><Image style={styles.image2} source={require('../assets/xjpx.png') }></Image></View>
-                <View style={styles.part_right}><Text style={styles.text2}>先进评选</Text></View>
-              </TouchableOpacity>
-
-              <TouchableOpacity style={[styles.row, styles.part_left]}>
-                <View style={styles.part_left}><Image style={styles.image2} source={require('../assets/more.png') }></Image></View>
-                <View style={styles.part_right}><Text style={styles.text2}>更多</Text></View>
-              </TouchableOpacity>
-            </View>
+        <View style={styles.block2}>
+          {secondRowViews[5]}
+          <View style={styles.row3}>
+            {secondRowViews[6]}
+            {secondRowViews[7]}
+            {secondRowViews[8]}
+            {secondRowViews[9]}
           </View>
         </View>
       </ScrollView>
@@ -124,30 +94,37 @@ export default class Home extends Component {
   }
 }
 
+
+
 var styles = StyleSheet.create({
   container: {
     marginTop: Platform.OS === 'ios' ? 64 : 51,
-    //alignItems: 'center',
     backgroundColor: '#d0d0d0',
     flex: 1,
-    //justifyContent: 'center',
   },
   row: {
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    //borderBottomWidth: 1,
     borderLeftWidth: 1,
     borderColor: '#d0d0d0',
+    flex: 1,
   },
   row2: {
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    //borderBottomWidth: 1,
     borderLeftWidth: 1,
     borderColor: '#d0d0d0',
+  },
+  row3: {
+    flex: 2,
+    flexDirection: 'row',
+    height: 63,
+    borderBottomWidth: 1,
+    borderColor: '#d0d0d0',
+    flexWrap: 'wrap'
   },
   image: {
     marginLeft: 15,
@@ -166,13 +143,13 @@ var styles = StyleSheet.create({
     height: 66,
     resizeMode: Image.resizeMode.contain
   },
-  list: {
+  block1: {
     height: 80,
     marginTop: 6,
     flexDirection: 'row',
     backgroundColor: '#ffffff',
   },
-  list2: {
+  block2: {
     height: 126,
     marginTop: 6,
     flexDirection: 'row',
@@ -191,10 +168,7 @@ var styles = StyleSheet.create({
     fontSize: 16,
     //fontFamily: 'HiraginoSans-W3',
   },
-  part_left: {
+  flex_1: {
     flex: 1,
-  },
-  part_right: {
-    flex: 1,
-  },
+  }
 });

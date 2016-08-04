fac0441c046f971a1b8abde3c693020db010131f
forrest23 <forrest23@126.com>
2016-08-04 14:25:11 +0800

    患者管理界面

--- app/containers/patientManager.js
+++ app/containers/patientManager.js
@@ -8,6 +8,9 @@ import {pageJump} from '../config/pageJump';
 const inPatientImg = require('../assets/inPatient.png');
 const outPatientImg = require('../assets/outPatient.png');
 const reservationImg = require('../assets/reservation.png');
+const heartImg = require('../assets/heart.png');
+const maleImg = require('../assets/male.png');
+const femaleImg = require('../assets/female.png');
 
 export default class PatientManager extends Component {
   render() {
@@ -27,6 +30,50 @@ export default class PatientManager extends Component {
             <Image style={styles.image} source={inPatientImg}></Image>
             <Text style={styles.text}>我的住院</Text>
           </TouchableOpacity>
+        </View>
+
+        <View style={styles.bottom}>
+          <View style={styles.bottomTitle}>
+            <View style={styles.title}>
+              <Image style={styles.image2} source={heartImg}></Image>
+              <Text style={styles.text2}>我的关注</Text>
+            </View>
+          </View>
+
+          <View style={[styles.row]}>
+            <TouchableOpacity style={[styles.part_left]}>
+              <Image style={styles.patinetImage} source={maleImg}></Image>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.part_right]}>
+              <View style={styles.rowText}>
+                <Text style={[styles.nameText, styles.flex3]}>罗德军</Text>
+                <Text style={[styles.nameText, styles.flex1]}>男</Text>
+                <Text style={[styles.nameText, styles.flex1]}>53岁</Text>
+              </View>
+              <View style={styles.rowText}>
+                <Text style={[styles.infoText, styles.flex2]}>骨科</Text>
+                <Text style={[styles.infoText, styles.flex3]}>先天性膝盖内翻</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+
+          <View style={[styles.row]}>
+            <TouchableOpacity style={[styles.part_left]}>
+              <Image style={styles.patinetImage} source={maleImg}></Image>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.part_right]}>
+              <View style={styles.rowText}>
+                <Text style={[styles.nameText, styles.flex3]}>张云张云张云</Text>
+                <Text style={[styles.nameText, styles.flex1]}>女</Text>
+                <Text style={[styles.nameText, styles.flex1]}>53岁</Text>
+              </View>
+              <View style={styles.rowText}>
+                <Text style={[styles.infoText, styles.flex2]}>耳鼻咽喉科</Text>
+                <Text style={[styles.infoText, styles.flex3]}>获得性免疫</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+
         </View>
       </View>
     );
@@ -58,11 +105,88 @@ var styles = StyleSheet.create({
     height: 34,
     resizeMode: Image.resizeMode.contain
   },
+  image2: {
+    width: 20,
+    height: 20,
+    resizeMode: Image.resizeMode.contain
+  },
   text: {
     marginTop: 8,
     fontSize: 17,
     color: '#34b5da'
     //fontFamily: 'HiraginoSans-W6',
   },
+  text2: {
+    marginLeft: 10,
+    fontSize: 19,
+    color: 'red'
+    //fontFamily: 'HiraginoSans-W6',
+  },
+  bottom:
+  {
+    backgroundColor: '#ffffff',
+    flex: 1,
+    marginTop: 10,
+  },
+  bottomTitle:
+  {
+    height: 45,
+    borderBottomWidth: 1,
+    borderColor: '#d0d0d0',
+  },
+  title:
+  {
+    alignItems: 'center',
+    flexDirection: 'row',
+    flex: 1,
+    marginLeft: 18,
+  },
+  row: {
+    flexDirection: 'row',
+    borderColor: '#ccc',
+    borderBottomWidth: 1,
+    height: 80,
+    alignItems: 'center',
+  },
+  part_left: {
+    flex: 1,
+    marginLeft: 12,
+  },
+  part_right: {
+    flex: 3,
+    marginLeft: 0,
+  },
+  patinetImage:
+  {
+    width: 60,
+    height: 60,
+  },
+  nameText:
+  {
+    fontSize: 18,
+  },
+  infoText:
+  {
+    fontSize: 16,
+    color:'#8E8E8E',
+  },
+  rowText: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
+    marginRight: 15,
+  },
+  flex1:
+  {
+    flex: 1,
+  },
+  flex2:
+  {
+    flex: 3,
+  },
+  flex3:
+  {
+    flex: 3,
+  }
 });
 
